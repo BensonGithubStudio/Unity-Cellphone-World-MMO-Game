2a8b394b6caa0c7f8cd30bbcd819e128c77fcc65
DESKTOP-N7BLO6Q\User <bensonletterbox@gmail.com>
2022-08-08 23:39:54 +0800

    設計網路連線架構，玩家可連網並進入MMO場景中

---
+++ Assets/Script/StartNetworkManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class StartNetworkManager : MonoBehaviourPunCallbacks
+{
+    public Animator ConnectingUI;
+    public Animator WaitingInGameUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConnectingUI.gameObject.SetActive(true);
+        WaitingInGameUI.gameObject.SetActive(false);
+
+        if (!PhotonNetwork.IsConnected)
+        {
+            ConnectingUI.SetBool("Is Connect", false);
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        else
+        {
+            DestroyConnectUI();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        ConnectingUI.SetBool("Is Connect", true);
+        Invoke("DestroyConnectUI", 1.2f);
+    }
+
+    void DestroyConnectUI()
+    {
+        ConnectingUI.gameObject.SetActive(false);
+    }
+
+    public void OnClickStartGame()
+    {
+        WaitingInGameUI.gameObject.SetActive(true);
+        PhotonNetwork.JoinRoom("World");
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        PhotonNetwork.CreateRoom("World");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        WaitingInGameUI.SetBool("Is Ready", true);
+        Invoke("GoInGame", 0.5f);
+    }
+
+    void GoInGame()
+    {
+        SceneManager.LoadScene("World Scene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
