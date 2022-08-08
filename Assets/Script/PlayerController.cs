c6cc9e9d2994c3c7a9113c38bb4c2fc6b493e5f1
DESKTOP-N7BLO6Q\User <bensonletterbox@gmail.com>
2022-08-09 00:44:01 +0800

    當進入遊戲場景時，玩家會隨機生成

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerController : MonoBehaviourPunCallbacks
+{
+    public GameObject Player;
+    public PhotonView pv;
+    public GameObject Cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pv.IsMine)
+        {
+            CameraCheck();
+        }
+    }
+
+    void CameraCheck()
+    {
+        GameObject[] Cams = GameObject.FindGameObjectsWithTag("MainCamera");
+        foreach (GameObject cam in Cams)
+        {
+            if (cam.GetComponent<PhotonView>().IsMine)
+            {
+                Cam = cam;
+            }
+            else
+            {
+                Destroy(cam);
+            }
+        }
+
+        Cam.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + 10, Player.transform.position.z);
+    }
+}
---
+++ Assets/Script/WorldNetworkManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class WorldNetworkManager : MonoBehaviour
+{
+    public int StartPositionCount;
+    public GameObject[] StartPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PhotonNetwork.IsConnected == false)
+        {
+            SceneManager.LoadScene("Start Scene");
+        }
+        else
+        {
+            int count = Random.Range(0, StartPositionCount);
+            PhotonNetwork.Instantiate("Player 1", StartPosition[count].transform.position, Quaternion.identity);
+            PhotonNetwork.Instantiate("Main Camera", StartPosition[count].transform.position, Quaternion.Euler(90, 0, 0));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
