58e36ed1d22ac83333705cab1cc08dcb41a8e00a
DESKTOP-N7BLO6Q\User <bensonletterbox@gmail.com>
2022-08-09 22:44:05 +0800

    新增功能 玩家可以在設定選項中選擇遊戲畫質

---
+++ Assets/Script/SettingControl.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+public class SettingControl : MonoBehaviour
+{
+    public GameObject SettingAllUI;
+    public GameObject QualitySettingUI;
+
+    void Start()
+    {
+        SettingAllUI.SetActive(false);
+
+        QualitySettingUI.SetActive(false);
+    }
+
+    public void OnClickLeaveGame()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public void OnClickOpenSetting()
+    {
+        SettingAllUI.SetActive(true);
+    }
+
+    public void OnClickCloseSetting()
+    {
+        SettingAllUI.SetActive(false);
+    }
+
+    public void OnClickQualitySetting()
+    {
+        if (QualitySettingUI.activeSelf)
+        {
+            QualitySettingUI.SetActive(false);
+        }
+        else
+        {
+            QualitySettingUI.SetActive(true);
+        }
+    }
+
+    public void OnClickVeryLowQuality()
+    {
+        QualitySettings.SetQualityLevel(0);
+    }
+    public void OnClickLowQuality()
+    {
+        QualitySettings.SetQualityLevel(1);
+    }
+    public void OnClickMediumQuality()
+    {
+        QualitySettings.SetQualityLevel(2);
+    }
+    public void OnClickHighQuality()
+    {
+        QualitySettings.SetQualityLevel(3);
+    }
+    public void OnClickVeryHighQuality()
+    {
+        QualitySettings.SetQualityLevel(4);
+    }
+    public void OnClickUltraQuality()
+    {
+        QualitySettings.SetQualityLevel(5);
+    }
+}
