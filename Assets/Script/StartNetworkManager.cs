21f04e7d3eb1ed82176b507c2cad2ebd6556a65a
DESKTOP-N7BLO6Q\User <bensonletterbox@gmail.com>
2022-08-11 14:14:24 +0800

    介面設計 增加角色資訊介面 增加一些動畫

--- Assets/Script/StartNetworkManager.cs
+++ Assets/Script/StartNetworkManager.cs
@@ -16,6 +16,8 @@ public class StartNetworkManager : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     void Start()
     {
+        QualitySettings.SetQualityLevel(5);
+
         StartButton.interactable = false;
         ConnectingUI.gameObject.SetActive(true);
         WaitingInGameUI.gameObject.SetActive(false);
